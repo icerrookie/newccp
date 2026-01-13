25aa4106138ccda15fd7934edbdd999dd7a464f7
icerrookie <105347592+icerrookie@users.noreply.github.com>
2026-01-13 12:37:14 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/idiom_study/',
+  base: '/newccp/',
   server: {
     host: '0.0.0.0'
   },
